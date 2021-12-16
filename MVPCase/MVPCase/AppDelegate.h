4372a7629a730775cadb2ba3bf9aa7f7928d34e8
chaomeng <custom_mail@163.com>
2021-12-16 15:02:06 +0800

    项目结构修改

---
+++ MVPCase/MVPCase/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  MVPCase
+//
+//  Created by YongQin on 2021/12/15.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
