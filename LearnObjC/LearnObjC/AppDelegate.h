270280e74f834a95d273d1288f74e7fccfc03281
chaomeng <custom_mail@163.com>
2022-06-05 21:02:47 +0800

    添加文件

---
+++ LearnObjC/LearnObjC/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  LearnObjC
+//
+//  Created by czeras on 2022/6/5.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ LearnObjC/LearnObjC/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  LearnObjC
+//
+//  Created by czeras on 2022/6/5.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
